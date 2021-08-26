633f5a9676104e8082f37ed2d2bfd1b26f5a30bf
Srijana Thulung <srijana.thulung123@gmail.com>
2021-08-26 10:39:36 +0545

    basic validation

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,7 +1,25 @@
+import { useState } from 'react';
 
 const Basicform = () => {
+  const [enteredName, setEnteredName] = useState('');
+  const [enterNameTouched, setEnteredNameTouch] = useState(false);
+
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+  const nameInputBlurHandler = () => {
+    setEnteredNameTouch(true);
+   };
+
   const formSubmissionHandler = event => {
     event.preventDefault();
+    console.log(enteredName);
+    if (enteredName.trim() == '') {
+      return;
+    }
+    setEnteredName('');
+    console.log(enteredName);
   };
 
     return (
@@ -9,7 +27,14 @@ const Basicform = () => {
           <div className='control-group'>
         <div className='form-control'>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+            <input
+              type='text'
+              id='name'
+              onChange={nameInputChangeHandler}
+              onBlur={nameInputBlurHandler}
+              value={enteredName}
+            />
+          
         </div>
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
