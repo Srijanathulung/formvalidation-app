2d8882cb14123bfc89bb3e680f433794a5762884
Srijana Thulung <srijana.thulung123@gmail.com>
2021-08-31 10:37:34 +0545

    useReducer is implemented

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,27 +1,45 @@
-import { useState } from 'react';
+//IMPLEMENTING 'useReducer'
+
+import { useReducer } from 'react';
+
+const initialInputState = {
+    value: '',
+    isTouch: true
+}
+
+const inputStateReducer = (state, action) => {
+    if (action.type == 'INPUT') {
+        return { value: action.value, isTouched: state.isTouched };
+    }
+    if (action.type == 'BLUR') {
+        return { isTouched: true, value: state.value };
+    }
+    if (action.type == 'RESET') {
+        return{isTouched:false,value:''}
+    }
+    return inputStateReducer;
+}
 const useInput = (validateValue) => {
 
-    const [enteredValue, setEnteredValue] = useState('');//enteredValue is passed to the SimpleInputComponent as the value of useInput value i.e(useInput(value=>value.trim()!==''))
-    const [isTouched, setIsTouched] = useState(false);
+   const[inputState,dispatch]= useReducer(inputStateReducer, initialInputState);
 
     
-    const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+    const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+      dispatch({ type: 'INPUT', value: event.target.value });
     };
     
     const valueBlurHandler = (event) => {
-        setIsTouched(true);
+        dispatch({type:'BLUR'})
     };
     
     const reset = () => {
-        setEnteredValue('');
-        setIsTouched(false);
+        dispatch({type:'RESET'})
      };
     return {
-        value: enteredValue,
+        value: inputState.value,
         isValid:valueIsValid,
         hasError: hasError,
         valueChangeHandler,
@@ -30,3 +48,37 @@ const useInput = (validateValue) => {
     };
 };
 export default useInput;
+
+// USED 'useState'
+// import { useState } from 'react';
+// const useInput = (validateValue) => {
+
+//     const [enteredValue, setEnteredValue] = useState('');//enteredValue is passed to the SimpleInputComponent as the value of useInput value i.e(useInput(value=>value.trim()!==''))
+//     const [isTouched, setIsTouched] = useState(false);
+
+    
+//     const valueIsValid = validateValue(enteredValue);
+//   const hasError = !valueIsValid && isTouched;
+
+//   const valueChangeHandler = (event) => {
+//     setEnteredValue(event.target.value);
+//     };
+    
+//     const valueBlurHandler = (event) => {
+//         setIsTouched(true);
+//     };
+    
+//     const reset = () => {
+//         setEnteredValue('');
+//         setIsTouched(false);
+//      };
+//     return {
+//         value: enteredValue,
+//         isValid:valueIsValid,
+//         hasError: hasError,
+//         valueChangeHandler,
+//         valueBlurHandler,
+//         reset
+//     };
+// };
+// export default useInput;
